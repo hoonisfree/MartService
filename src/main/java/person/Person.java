cc185ae576b1ddc5bcbef844bf33c35481bcbe4c
moon-choi <choimy1991@gmail.com>
2024-03-24 11:38:34 +0900

    Cart 맵 로직 개선 후 Cashier, Kiosk에도 수정된 맵 로직 반영

--- src/main/java/person/Person.java
+++ src/main/java/person/Person.java
@@ -11,7 +11,4 @@ public class Person {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 }
--- src/main/java/thing/Bill.java
+++ src/main/java/thing/Bill.java
@@ -5,6 +5,7 @@ import product.Product;
 
 public class Bill {
     private Map<Product, Integer> cartItems;
+
     public Bill(Map<Product, Integer> cartItems) {
         this.cartItems = cartItems;
     }
